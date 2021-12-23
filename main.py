c906eed40bc88482a203727db239ad8846e6ffe6
Prathima Kadari <kadariprathima4@gmail.com>
2021-12-23 23:14:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ st.sidebar.markdown('<h1 style="margin-left:8%; color:#D19487 ">Nakagawa Dashboa
 
 add_selectbox = st.sidebar.radio(
     "",
-    ("Home", "About", "Features", "Safest Path", "Maps", "Visualization", "Conclusion", "Team")
+    ("Home", "About", "Features", "Safest Path", "Maps", "Visualizations", "Conclusion", "Team")
 )
 
 if add_selectbox == 'Home':
@@ -69,7 +69,7 @@ if add_selectbox == 'Home':
           unsafe_allow_html=True
     )
     
-    st.markdown('<h4>Problem Statement</h4>', unsafe_allow_html=True)
+    st.subheader('PROBLEM STATEMENT')
     
     st.markdown('Natural Disasters are problems in Japan, with risk of earthquakes, floods and tsunamis. Japan has well-developed \
         disaster response systems, but densely populated cities and narrow roads make managing the response difficult. By giving \
@@ -81,12 +81,17 @@ elif add_selectbox == 'About':
     st.subheader('ABOUT THE PROJECT')
 
     st.markdown('<h4>Project Goals</h4>', unsafe_allow_html=True)
-    st.markdown('– collect satellite images and identify road characteristics', unsafe_allow_html=True) 
-    st.markdown('– build a model for scoring the roads in terms of their suitability for use in emergency', unsafe_allow_html=True) 
-    st.markdown('– build a pathfinding model from A to B, combining it with road characteristics', unsafe_allow_html=True) 
-    st.markdown('– suggest safest path from A to B', unsafe_allow_html=True) 
-    st.markdown('– publish interactive dashboards to display road characteristics and safest paths', unsafe_allow_html=True) 
-    st.markdown('– arrange demonstration and publicise to local audiences', unsafe_allow_html=True) 
+    st.markdown('• collect satellite images and identify road characteristics', unsafe_allow_html=True) 
+    st.markdown('• build a model for scoring the roads in terms of their suitability for use in emergency', unsafe_allow_html=True) 
+    st.markdown('• build a pathfinding model from A to B, combining it with road characteristics', unsafe_allow_html=True) 
+    st.markdown('• suggest safest path from A to B', unsafe_allow_html=True) 
+    st.markdown('• publish interactive dashboards to display road characteristics and safest paths', unsafe_allow_html=True) 
+    st.markdown('• arrange demonstration and publicise to local audiences', unsafe_allow_html=True) 
+    
+    st.markdown('<h4>Location Choosen</h4>', unsafe_allow_html=True)
+    st.markdown('We had choosen "Nakagawa-Ku as our region of interest, which comes under Aichi prefecture of Nagoya City. It comes under Chubu region and \
+        is the 4th densely populated city in Japan with high risk prone to disasters.',
+                unsafe_allow_html=True)
     
     st.markdown('<h4>Developments Made</h4>', unsafe_allow_html=True)
     st.markdown('We had designed a model collecting data about the local roads from satellite images, classify them and indicate the safest \
@@ -99,10 +104,11 @@ elif add_selectbox == 'About':
     
 elif add_selectbox == 'Features':
 
-    st.markdown('<h4>Project Endorsements</h4>', unsafe_allow_html=True)
-    st.markdown('Point 1', unsafe_allow_html=True)
-    st.markdown('Point 2', unsafe_allow_html=True)
-    st.markdown('Point 3', unsafe_allow_html=True)
+    st.subheader('PROJECT ENDORSEMENTS')
+
+    st.markdown('Safest route path to take at occurences of japan disasters', unsafe_allow_html=True)
+    st.markdown('Locates Shelters in Nakagawa Ward - Earthquakes, Tsunamis and Floods', unsafe_allow_html=True)
+    st.markdown('Visualizations to Check and Differentiate Parameters across the Nakagawa Ward', unsafe_allow_html=True)
     
 elif add_selectbox == 'Safest Path':
     st.subheader('Find Safest Path')
@@ -185,6 +191,61 @@ elif add_selectbox == 'Maps':
         # m = folium.Map(location=london_location, zoom_start=15)
         folium_static(m, width=900)
 
+        
+elif add_selectbox == 'Visualizations':
+    
+    st.subheader('ABOUT THE PROJECT')
+
+    st.markdown('<h4>Project Goals</h4>', unsafe_allow_html=True)
+    st.markdown('• collect satellite images and identify road characteristics', unsafe_allow_html=True) 
+    st.markdown('• build a model for scoring the roads in terms of their suitability for use in emergency', unsafe_allow_html=True) 
+    st.markdown('• build a pathfinding model from A to B, combining it with road characteristics', unsafe_allow_html=True) 
+    st.markdown('• suggest safest path from A to B', unsafe_allow_html=True) 
+    st.markdown('• publish interactive dashboards to display road characteristics and safest paths', unsafe_allow_html=True) 
+    st.markdown('• arrange demonstration and publicise to local audiences', unsafe_allow_html=True) 
+    
+    st.markdown('<h4>Location Choosen</h4>', unsafe_allow_html=True)
+    st.markdown('We had choosen "Nakagawa-Ku as our region of interest, which comes under Aichi prefecture of Nagoya City. It comes under Chubu region and \
+        is the 4th densely populated city in Japan with high risk prone to disasters.',
+                unsafe_allow_html=True)
+    
+    st.markdown('<h4>Developments Made</h4>', unsafe_allow_html=True)
+    st.markdown('We had designed a model collecting data about the local roads from satellite images, classify them and indicate the safest \
+        route to be taken from point A to point B and an interactive dashboard to display the safest route in a map.',
+                unsafe_allow_html=True)
+    st.markdown('By making individuals aware, it will improve their preparedness and it can be used within families to prepare disaster \
+        response plans, depending on their circumstances. To be used by individuals, families and groups, and foreign residents who may \
+            not understand local information. Further development will be covering more geographical areas and publicising on a local level.'
+                , unsafe_allow_html=True)
+    
+    
+elif add_selectbox == 'Conclusion':
+    
+    st.subheader('ABOUT THE PROJECT')
+
+    st.markdown('<h4>Project Goals</h4>', unsafe_allow_html=True)
+    st.markdown('• collect satellite images and identify road characteristics', unsafe_allow_html=True) 
+    st.markdown('• build a model for scoring the roads in terms of their suitability for use in emergency', unsafe_allow_html=True) 
+    st.markdown('• build a pathfinding model from A to B, combining it with road characteristics', unsafe_allow_html=True) 
+    st.markdown('• suggest safest path from A to B', unsafe_allow_html=True) 
+    st.markdown('• publish interactive dashboards to display road characteristics and safest paths', unsafe_allow_html=True) 
+    st.markdown('• arrange demonstration and publicise to local audiences', unsafe_allow_html=True) 
+    
+    st.markdown('<h4>Location Choosen</h4>', unsafe_allow_html=True)
+    st.markdown('We had choosen "Nakagawa-Ku as our region of interest, which comes under Aichi prefecture of Nagoya City. It comes under Chubu region and \
+        is the 4th densely populated city in Japan with high risk prone to disasters.',
+                unsafe_allow_html=True)
+    
+    st.markdown('<h4>Developments Made</h4>', unsafe_allow_html=True)
+    st.markdown('We had designed a model collecting data about the local roads from satellite images, classify them and indicate the safest \
+        route to be taken from point A to point B and an interactive dashboard to display the safest route in a map.',
+                unsafe_allow_html=True)
+    st.markdown('By making individuals aware, it will improve their preparedness and it can be used within families to prepare disaster \
+        response plans, depending on their circumstances. To be used by individuals, families and groups, and foreign residents who may \
+            not understand local information. Further development will be covering more geographical areas and publicising on a local level.'
+                , unsafe_allow_html=True)
+    
+    
 elif add_selectbox == 'Team':
     st.subheader('Collaborators')
 
