e7910e516ea0a3a3489565aa0c0b63c01e2f7657
Shalini GJ <65597165+shalinigj@users.noreply.github.com>
2021-12-28 13:10:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -166,10 +166,9 @@ elif add_selectbox == 'Maps':
     
     
         ward = ward_type.split(" ")
-        #ward_n = ward[1]
-        #st.write(ward_n)
-        #ward_split = str(ward_n).replace("("," ")
-        #st.write(ward_split)
+        ward_n = ward[1]
+        ward_split = str(ward_n).replace("("," ")
+        st.write(ward_split)
         #details = map_data[map_data['ward']==ward[1]]
       
        # coordinates = {
