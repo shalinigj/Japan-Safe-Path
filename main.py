d263c41d7189470247e7adf02d296a69e5ec5dde
Shalini GJ <65597165+shalinigj@users.noreply.github.com>
2021-12-28 13:14:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -166,11 +166,11 @@ elif add_selectbox == 'Maps':
     
     
         ward = ward_type.split(" ")
-        ward_n = ward[1]
-        ward_split = str(ward_n).replace("("," ")
-        details = map_data[map_data['ward']==ward_split]
+        ward_split = str(ward[1]).replace("("," ")
+        st.write(ward_split)
+        #details = map_data[map_data['ward']==ward_split]
       
-        coordinates = {
+        #coordinates = {
         
            '中川区 (Nakagawa Ward)': [35.1392027, 136.7778013],
             '緑区 (Midori Ward)': [35.0852, 136.9708]
